da16725c4494b393958b8205160c2b909493c0bb
chosson <chosson@users.noreply.github.com>
2021-11-02 14:31:50 -0400

    Added lots of comments and documentation on the tasks to do.

--- README.md
+++ README.md
@@ -15,14 +15,13 @@ Au lieu d'avoir ceci dans le code:
 english_names = ["C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
 solfeggio_names = ["Do", "Réb", "Ré", "Mib", "Mi", "Fa", "Fa#", "Sol", "Lab", "La", "Sib","Si"]
 chords = {
-	"Do majeur" : ("Do", "Mi", "Sol"),
-	"Fa majeur" : ("Fa", "La", "Do"),
-	"Sol majeur" : ("Sol", "Si", "Ré"),
-	"La mineur" : ("La", "Do", "Mi")
+    "Do majeur": ("Do", "Mi", "Sol"),
+    "Fa majeur": ("Fa", "La", "Do"),
+    "Sol majeur": ("Sol", "Si", "Ré"),
+    "La mineur": ("La", "Do", "Mi")
 }
 ```
 Nous avons un JSON comme ceci:
-Exemple :
 ```json
 {
   "english_names": [ "C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" ],
@@ -52,12 +51,171 @@ Exemple :
 }
 ```
 
+Il nous faut donc charger le contenu de ce fichier et ensuite l'utiliser de la même façon que dans l'exercice du chapitre 7.2, c'est-à-dire dans la fonction `build_note_dictionnaries`
+
+```python
+notes_data = CHARGER LE CONTENU DU JSON
+note_names = CE QU'IL Y A DANS "solfeggio_names"
+midi_to_name, name_to_midi = build_note_dictionaries(note_names)
+chords = CE QU'IL Y A DANS "chords"
+```
+
 ## 2. Configuration d'actions sur boutons de manettes
 
-Il faut suivre la démonstration faite en classe. Voici les étapes :
+Cet exercice est somme toute assez compliqué, et il y a définitivement plusieurs façons d'en arriver à une solution.
 
-#### 2.1. Structure d'un fichier INI
+On veut ultimement pouvoir faire jouer des notes et des accords sur un piano en sortie à partir de boutons sur une manette de jeu en entrée. Par exemple, le bouton A joue un do majeur, le bouton X joue un ré mineur, le bouton R joue un do, et ainsi de suite.
 
-#### 2.2. Construire des callbacks à appeler sur des boutons
+On veut aussi pouvoir exécuter des actions quelquonques (des fonctions) sur les boutons de la manette.
 
-#### 2.3. Charger la configuration et exécuter un code
+### 2.1. Structure d'un fichier INI
+
+Les fichiers INI, ou de façon plus générale les fichiers de *config* ([plus d'info](https://fr.wikipedia.org/wiki/Fichier_INI)), permettent d'enregistrer des configurations dans un format assez simple. La forme générale est :
+
+```ini
+[Nom d'une section]
+Variable = La valeur de la variable
+UneAutreVariable = Une autre valeur
+; Un commentaire (les commentaires commencent par un point-virgule)
+
+[Nom d'une autre section]
+Variable = La valeur de la variable
+```
+
+Nous allons nous servir d'un fichier INI pour contenir la configuration des contrôles. C'est en fait une pratique assez courante dans le domaine des jeux vidéos d'utiliser ce format de fichier pour les options de jeux de toute sorte. Dans notre cas, nous avons un fichier qui ressemble à ceci : 
+
+```ini
+[gamepad]
+BTN_TR = Do4
+BTN_SOUTH = Do majeur
+BTN_WEST = Ré mineur
+BTN_EAST = La mineur
+BTN_NORTH = Sol majeur
+BTN_TL = sustain
+```
+
+Le nom de la section qui va contenir nos contrôle est `gamepad`. S'il y a d'autres sections présentes dans le fichier, elles seront ignorées. Dans cette section, chaque ligne représente un bouton et la commande associée. Par exemple, `BTN_SOUTH` (qui est le bouton A sur une manette de Xbox) va jouer un do majeur et le `BTN_TL` (bouton LB sur manette Xbox) va activer la pédale de résonnance.
+
+Il nous faut donc passer sur toutes les configurations de boutons dans le fichier et les ajouter dans un dictionnaire où la clé est le nom du bouton et la valeur est l'action à exécuter. Nous allons faire des fonctions de rappel pour contenir ces actions. Il nous faut donc construire des callbacks qui peuvent, par exemple, jouer un accord.
+
+### 2.2. Construire des callbacks à appeler sur des boutons
+
+Il y a plusieurs types de callbacks que nous avons besoin de construire : ceux qui jouent une note unique, ceux qui jouent un accord (plusieurs notes en même temps) et ceux qui exécutent une action quelquonque.
+
+#### 2.2.1 Note unique
+
+La fonction `build_note_callbacks` prend en paramètre le nom de la note à jouer, le dictionnaire de conversion de nom de note à numéro MIDI et la liste des sortie MIDI sur lesquelles envoyer la note. On retourne deux fonctions sans paramètre qui appuie et relâche, respectivement, la note donnée sur les sorties données. Voici un exemple d'utilisation de cette fonction :
+
+```python
+note_names = ["Do", "Réb", "Ré", "Mib", "Mi", "Fa", "Fa#", "Sol", "Lab", "La", "Sib", "Si"]
+midi_to_name, name_to_midi = build_note_dictionaries(note_names)
+midi_outputs = [
+    mido.open_output("Le port MIDI d'un piano"),
+    mido.open_output("Un autre port MIDI")
+]
+# On obtient nos deux callbacks qui jouent do 4 (le do du milieu)
+note_on, note_off = build_note_callbacks("Do4", name_to_midi, midi_outputs)
+# `note_on` appuie sur un do
+note_on()
+time.sleep(1.0)
+# On relâche do
+note_off()
+```
+On remarque que les fonctions de rappel ne prennent rien en paramètre et connaissent leur note à travers leur fermeture lexicale.
+
+#### 2.2.2 Accord
+
+Dans `build_chord_callbacks` on fait essentiellement la même chose que dans `build_note_callbacks`, mais en jouant plusieurs notes en même temps. On prend donc en paramètre une liste de noms de note à jouer plutôt qu'une note unique. Exemple d'utilisation : 
+
+```python
+note_names = ["Do", "Réb", "Ré", "Mib", "Mi", "Fa", "Fa#", "Sol", "Lab", "La", "Sib", "Si"]
+midi_to_name, name_to_midi = build_note_dictionaries(note_names)
+midi_outputs = [
+    mido.open_output("Le port MIDI d'un piano"),
+    mido.open_output("Un autre port MIDI")
+]
+# On obtient nos deux callbacks qui jouent un do majeur (do, mi, sol)
+chord_on, chord_off = build_chord_callbacks(("Do3", "Mi3", "Sol3"), name_to_midi, midi_outputs)
+# `chord_on` appuie un do majeur (do, mi, sol)
+chord_on()
+time.sleep(1.0)
+# On relâche do majeur
+chord_off()
+```
+
+#### 2.2.3 Action personnalisée
+
+On veut aussi pouvoir exécuter des fonctions quelquonques sur un bouton de manette. `build_custom_action_callbacks` prend en paramètre le nom de l'action qui sera écrite dans le fichier de commandes, le dictionnaire des actions personnalisées et la liste des sorties MIDI à utiliser. On retourne deux fonctions de rappel sans paramètres qui peuvent être à `None` s'il n'y a rien à faire dans un cas ou dans l'autre. Les fonctions dans le dictionnaire doivent prendre en paramètre les sorties MIDI. Voici un exemple d'utilisation où on veut créer deux callbacks qui affichent des messages dans la console :
+
+```python
+# Dans cet exemple, on ne fait rien avec la sortie MIDI
+def foo0(midi_outputs):
+    print("henlo")
+def foo1(midi_outputs):
+    print("k bye")
+
+# Dans le dictionnaire, la clé est le nom de la commande, et la valeur est un dictionnaire où
+# la clé est True pour appuyé et False pour relâché et la valeur est une fonction à exécuter.
+custom_actions = {
+    "foo": {True: foo0, False: foo1}
+}
+action_on, action_off = build_custom_action_callbacks("foo", custom_actions, [])
+# On affiche hello
+action_on()
+# On affiche bye
+action_off()
+```
+
+Là, on pourrait se demander pourquoi on lie toujours les sorties MIDI aux callbacks plutôt que de créer des callbacks qui prennent en paramètre les sorties et les passer à chaque fois qu'on les appelle. C'est tout simplement un choix qu'on a fait, mais les deux façons de faire sont toutes aussi valides, elles offrent des options différentes.
+
+### 2.3. Charger la configuration
+
+Maintenant que nous pouvons construire tous les types de callback dont nous avons besoin, on peut lire le fichier INI et créer notre dictionnaire de contrôles. La fonction `load_input_mappings` prend en paramètre le nom du fichier de configuration et les autres trucs nécessaire à la construction des callbacks. Le dictionnaire retourné a la forme :
+
+```python
+mappings = {
+    "Nom d'un bouton de la manette": {
+        True: <Callback à appeler quand le bouton est appuyé>,
+        False: <Callback à appeler quand le bouton est relâché>
+    },
+    "Nom d'un autre bouton de la manette": {
+        True: <Callback à appeler quand le bouton est appuyé>,
+        False: <Callback à appeler quand le bouton est relâché>
+    },
+    Et ainsi de suite...
+}
+```
+
+Par exemple, le fichier INI :
+
+```ini
+[gamepad]
+BTN_SOUTH = Do majeur
+BTN_WEST = Do4
+BTN_EAST = foo
+```
+
+donnerait le dictionnaire suivant (en posant qu'on a l'action `foo` de l'exemple de 2.2.3) :
+
+```python
+mappings = {
+    "btn_south": {
+        True: <Callback qui joue do majeur>,
+        False: <Callback relâche do majeur>
+    },
+    "btn_west": {
+        True: <Callback qui joue do 4>,
+        False: <Callback relâche do 4>
+    },
+    "btn_east": {
+        True: <Callback qui affiche "henlo">,
+        False: <Callback qui affiche "k bye">
+    }
+}
+```
+
+On met tout en minuscule pour ignorer la casse.
+
+## 3. Exécuter la configuration
+
+On fait une boucle infinie qui traite les événements de la manette de jeu et qui exécute les actions associées. Remarquez que dans ce cas-ci, on fait du *polling*, même si on a des fonctions de rappel. En effet, dans notre les fonction de rappel permettent tout simplement d'avoir un dictionnaire qui associe les boutons à des actions, sans que ce soit nécessairement un exécution en parallèle.
--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import mido
 import json
 import csv
 import os
 import time
 import configparser
-from copy import deepcopy
 
 import inputs
 import mido
 
 
 notes_per_octave = 12
+default_velocity = 80
 
 
 def build_note_dictionaries(note_names, add_octave_no=True):
@@ -21,37 +20,51 @@ def build_note_dictionaries(note_names, add_octave_no=True):
 
 	midi_to_name = {}
 	name_to_midi = {}
-	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8
+	# Pour chaque octave de 0 à 8 (inclus). On va générer tout l'octave 8, même si la dernière note du piano est Do 8.
 	for octave in range(8+1):
-		# Pour chaque note de l'octave
+		# Pour chaque note de l'octave :
 		for note in range(notes_per_octave):
-			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires
+			# Calculer le numéro MIDI de la note et ajouter aux deux dictionnaires.
 			midi_no = c0_midi_no + octave * notes_per_octave + note
-			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai
+			# Ajouter le numéro de l'octave au nom de la note si add_octave_no est vrai.
 			full_note_name = note_names[note] + (str(octave) if add_octave_no else "")
 			midi_to_name[midi_no] = full_note_name
-			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux
+			# Garder les numéros de notes dans name_to_midi entre 0 et 11 si add_octave_no est faux.
 			name_to_midi[full_note_name] = midi_no if add_octave_no else midi_no % notes_per_octave
 	return midi_to_name, name_to_midi
 
 def send_note_on(note_name, name_to_midi, midi_outputs):
+	# On construit un message MIDI qui appuie la note.
 	msg = mido.Message("note_on", note=name_to_midi[note_name], velocity=default_velocity)
+	# On envoie le message sur chaque sortie.
 	for o in midi_outputs:
 		o.send(msg)
 
 def send_note_off(note_name, name_to_midi, midi_outputs):
+	# On construit un message MIDI qui relâche la note.
 	msg = mido.Message("note_off", note=name_to_midi[note_name])
+	# On envoie le message sur chaque sortie.
 	for o in midi_outputs:
 		o.send(msg)
 
 def build_note_callbacks(note_name, name_to_midi, midi_outputs):
-	# Construire des callbacks pour bouton appuyé et relâché en utilisant `send_note_on` et `send_note_off`
-	# Ces callbacks ne prennent aucun paramètre
+	# Construire des callbacks pour bouton appuyé et relâché (retournés en tuple) en utilisant `send_note_on` et `send_note_off`.
+	# Ces callbacks ne prennent aucun paramètre.
+	def action_fn_pressed():
+		send_note_on(note_name, name_to_midi, midi_outputs)
+	def action_fn_released():
+		send_note_off(note_name, name_to_midi, midi_outputs)
 	return action_fn_pressed, action_fn_released
 
 def build_chord_callbacks(chord, chord_notes, name_to_midi, midi_outputs):
 	# Construire des callbacks pour bouton appuyé et relâché en utilisant `send_note_on` et `send_note_off`. On veut appuyer ou relâcher chaque note de l'accord.
-	# Ces callbacks ne prennent aucun paramètre
+	# Ces callbacks ne prennent aucun paramètre.
+	def action_fn_pressed():
+		for note in chord_notes[chord]:
+			send_note_on(note, name_to_midi, midi_outputs)
+	def action_fn_released():
+		for note in chord_notes[chord]:
+			send_note_off(note, name_to_midi, midi_outputs)
 	return action_fn_pressed, action_fn_released
 
 def build_custom_action_callbacks(action_name, custom_actions, midi_outputs):
@@ -59,25 +72,50 @@ def build_custom_action_callbacks(action_name, custom_actions, midi_outputs):
 	# Chaque action personnalisée est une fonction qui prend en paramètre une liste de sorties MIDI
 	# Par exemple, la clé "foo" dans le dictionnaire serait elle-même un dictionnaire de la forme {True: une_fonction, False: une_autre_fonction}, où `une_fonction` doit être appelée quand le bouton de la manette est appuyé, et `une_autre_fonction` est appuyée quand le bouton est relâché.
 	# Construire des callbacks pour bouton appuyé et relâché.
+	pressed_fn, released_fn = None, None
+	# Si l'action a une fonction donnée pour bouton appuyé :
+	if True in custom_actions[action_name] and custom_actions[action_name][True] is not None:
+		# On lie cette fonction aux sorties MIDI.
+		def action_fn_pressed():
+			custom_actions[action_name][True](midi_outputs)
+		pressed_fn = action_fn_pressed
+	# Si l'action a une fonction donnée pour bouton relâché :
+	if False in custom_actions[action_name] and custom_actions[action_name][False] is not None:
+		# On lie cette fonction aux sorties MIDI.
+		def action_fn_released():
+			custom_actions[action_name][False](midi_outputs)
+		released_fn = action_fn_released
 	return pressed_fn, released_fn
 
 def load_input_mappings(filename, name_to_midi, chord_notes, midi_outputs, custom_actions={}):
 	# On ouvre et lit le fichier INI
+	config = configparser.ConfigParser()
+	config.read(filename)
 	# On prend la section "gamepad" (qui est probablement la seule)
-	gamepad_section = {} # TODO
+	gamepad_section = config["gamepad"]
 
+	# On fait un dictionnaire 
 	mappings = {}
 	for gamepad_input in gamepad_section:
 		action_name = gamepad_section[gamepad_input]
+		# Construire des callbacks pour l'action appropriée et l'ajouter au mapping.
 		pressed_fn, released_fn = None, None
 		# On veut construire les callbacks appropriés selon le nom de l'action.
 		# Si l'action est un nom de note (présent dans `name_to_midi`):
+		if action_name in name_to_midi:
 			# On construit les callbacks qui joue une note
+			pressed_fn, released_fn = build_note_callbacks(action_name, name_to_midi, midi_outputs)
 		# Si l'action est un nom d'accord (présent dans `chord_notes`):
+		elif action_name in chord_notes:
 			# On construit les callbacks qui joue un accord
+			pressed_fn, released_fn = build_chord_callbacks(action_name, chord_notes, name_to_midi, midi_outputs)
 		# Si l'action est une action personnalisée (présent dans `custom_actions`):
+		elif action_name in custom_actions:
 			# On construit les callbacks qui exécutent une action personnalisée (une fonction présente dans le dictionnaire)
+			pressed_fn, released_fn = build_custom_action_callbacks(action_name, custom_actions, midi_outputs)
 		# On ajoute au dictionnaire des mappings où la clé est le nom du contrôle de manette (`gamepad_input`) et la valeur est un dictionnaire où on a les clés True et False et les valeurs pressed_fn et released_fn associées.
+		mappings[gamepad_input] = {True: pressed_fn, False: released_fn}
+
 	return mappings
 
 
@@ -88,42 +126,69 @@ def main():
 	print(f"Liste des ports d'entrée MIDI disponibles : {available_input_ports}")
 	print(f"Liste des ports de sortie MIDI disponibles : {available_output_ports}")
 
+	# On choisit une manette de jeu que la librairie inputs nous fournit.
 	gamepad = inputs.devices.gamepads[0]
-	midi_outputs = (mido.open_output("UM-ONE 3"), mido.open_output("UnPortMIDI 4"))
-	midi_input = mido.open_input("UM-ONE 0")
+	# On ouvre les sorties MIDI qui nous intéressent.
+	midi_outputs = [mido.open_output("3- UM-ONE 1")]
 
-	note_names = {} # Charger du JSON
-	midi_to_name, name_to_midi = build_note_dictionaries([]) # Charger du JSON
-	chords = {} # Charger du JSON
+	# On lit le fichier JSON qui contient les informations musicales.
+	notes_data = json.load(open("notes.json", "r", encoding="utf-8"))
+	# On extrait les noms des notes en français.
+	note_names = notes_data["solfeggio_names"]
+	# On extrait les notes des accords connus.
+	chords = notes_data["chords"]
+	# On construit nos dictionnaires de traduction de nom à MIDI et vice-versa.
+	midi_to_name, name_to_midi = build_note_dictionaries(note_names)
 
+	# On se crée deux actions custom qui ne font qu'afficher des messages
 	def foo0(midi_outputs):
 		print("henlo")
 	def foo1(midi_outputs):
 		print("k bye")
+
+	# On crée des actions custom qui contrôle la pédale de résonnance du piano
 	def sustain_on(midi_outputs):
+		# Message MIDI correspondant à la pédale appuyée.
 		msg = mido.Message("control_change", channel=0, control=64, value=127)
+		# On envoit sur chaque sortie.
 		for o in midi_outputs:
 			o.send(msg)
 	def sustain_off(midi_outputs):
+		# Message MIDI correspondant à la pédale relâchée.
 		msg = mido.Message("control_change", channel=0, control=64, value=0)
+		# On envoit sur chaque sortie.
 		for o in midi_outputs:
 			o.send(msg)
 
+	# Dans le dictionnaire d'actions, la clé est le nom de la commande, et la valeur est un dictionnaire.
+	# Dans celui-ci, la clé est True pour appuyé et False pour relâché et la valeur est une fonction à exécuter (ou None si rien à faire).
 	custom_actions = {
 		"foo": {True: foo0, False: foo1},
+		# Dans le fichier INI, la ligne `BTN_TR = sustain` ferait en sorte que d'appuyer le bouton LB va contrôler la pédale.
 		"sustain": {True: sustain_on, False: sustain_off}
 	}
 
+	# On charge la configuration du fichier INI.
 	mappings = load_input_mappings("input.ini", name_to_midi, chords, midi_outputs, custom_actions)
+	# On affiche le contenu juste pour nous aider.
+	print("mappings =", json.dumps(mappings, indent=2, default=str))
 
+	# Pour toujours...
 	while True:
+		# Pour chaque bouton de manette reçu :
 		for e in gamepad.read():
+			# On prend le code du bouton (son nom) et on en ignore la casse.
 			btn = e.code.lower()
+			# On prend l'état du bouton (1 = appuyé, 0 = relâché).
 			pressed = bool(e.state)
+			# Si c'est un bouton qu'on connait (dans notre configuration).
 			if btn in mappings:
+				# On obtient les callbacks associés.
 				callbacks = mappings[btn]
+				# On appelle le callback associé à l'état du bouton (si applicable).
 				if pressed in callbacks and callbacks[pressed] is not None:
 					mappings[btn][pressed]()
+				# On affiche le bouton et son état, juste pour nous aider.
 				print(btn, pressed)
 
 if __name__ == "__main__":
